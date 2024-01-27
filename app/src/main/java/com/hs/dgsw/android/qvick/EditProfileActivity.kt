cc76db797d39954c1f71a188c7abead6ea4e40a0
형언 <ilsin302a@gmail.com>
2024-01-27 22:34:18 +0900

    프로필 뷰 추가

---
+++ app/src/main/java/com/hs/dgsw/android/qvick/EditProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.hs.dgsw.android.qvick
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
