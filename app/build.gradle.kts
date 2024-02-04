1c20259315929ab4271ee76c0d9aa1250fbeeb08
형언 <ilsin302a@gmail.com>
2024-02-05 01:18:35 +0900

    mainfest 파일에 인터넷 퍼미션 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -54,4 +56,6 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+
+
 }
