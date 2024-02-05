61eb2e308a6dd60699c63b19b4b590a01eb0f1b4
형언 <ilsin302a@gmail.com>
2024-02-05 15:36:12 +0900

    구글로 로그인 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.gms:play-services-base:18.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -57,5 +58,6 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
 
-
+    // google login
+    implementation("com.google.android.gms:play-services-ads-base:22.6.0")
 }
--- app/src/main/java/com/hs/dgsw/android/qvick/LoginToDAuthActivity.kt
+++ app/src/main/java/com/hs/dgsw/android/qvick/LoginToDAuthActivity.kt
@@ -2,10 +2,15 @@ package com.hs.dgsw.android.qvick
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hs.dgsw.android.qvick.databinding.ActivityLoginToDauthBinding
 
 class LoginToDAuthActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityLoginToDauthBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_to_dauth)
+        setContentView(binding.root)
     }
 }
