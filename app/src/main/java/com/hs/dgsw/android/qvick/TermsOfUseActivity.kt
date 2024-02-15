4c45c9df008690758caa335cab1152c10b82448e
형언 <ilsin302a@gmail.com>
2024-02-15 17:05:20 +0900

    약관동의 로직 구현

--- app/src/main/java/com/hs/dgsw/android/qvick/TermsOfUseActivity.kt
+++ app/src/main/java/com/hs/dgsw/android/qvick/TermsOfUseActivity.kt
@@ -3,6 +3,7 @@ package com.hs.dgsw.android.qvick
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.hs.dgsw.android.qvick.databinding.ActivityTermsOfUseBinding
 
 class TermsOfUseActivity : AppCompatActivity() {
@@ -14,10 +15,33 @@ class TermsOfUseActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        // 전체동의 체크박스
+        binding.fullAgreementCB.setOnClickListener{
+            binding.fullAgreementCB.setOnClickListener {
+                if (!binding.termsOfServiceCB.isChecked) {
+                    binding.termsOfServiceCB.isChecked = true
+                }
+                if (!binding.PrivacyCB.isChecked) {
+                    binding.PrivacyCB.isChecked = true
+                }
+                if (!binding.PrivacyCB2.isChecked) {
+                    binding.PrivacyCB2.isChecked = true
+                }
+            }
+        }
+
+
         // 학번/기숙사 호실 입력 화면으로 이동
         binding.nextPageBtn.setOnClickListener {
-            intent = Intent(this, StudentIdActivity::class.java)
-            startActivity(intent)
+            if (binding.termsOfServiceCB.isChecked &&
+                binding.PrivacyCB.isChecked &&
+                binding.PrivacyCB2.isChecked){
+
+                intent = Intent(this, StudentIdActivity::class.java)
+                startActivity(intent)
+            } else{
+                Toast.makeText(applicationContext, "필수 항목을 모두 선택해 주세요", Toast.LENGTH_SHORT).show()
+            }
         }
         // 이전 버튼
         binding.backBtn.setOnClickListener {
